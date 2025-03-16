ead1a873bcce1246ae94fd65cf80a3fe9d38e6ea
Jônatas Quirino <105068717+ojonatasquirino@users.noreply.github.com>
2025-03-16 02:41:44 +0000

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,18 @@ export default function Page() {
   return (
     <section>
       <img
-        src="/avatar.jpg" // Substitua pelo caminho correto da imagem
+        src="/avatar.jpg"
         alt="Avatar de Jônatas Quirino"
-        className="w-16 h-16 rounded-full mb-4" // 24x24 é um tamanho padrão para avatares
+        className="w-16 h-16 rounded-full mb-4"
       />
-      <h1 className="mb-6 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
         Jônatas Quirino
       </h1>
-      <p className="mb-1">
-        {`Amigo de Cristo, marido de Vic e pai de Eva. Sintetizo o meu cérebro criando design mínimo, escrevendo software e percepções.`}
+      <p className="mb-3">
+        {`Sintetizo o meu cérebro criando design mínimo, escrevendo software e percepções.`}
       </p>
+      <h3 className="mb-6 text-xl tracking-tighter"> Recentes ⇣ </h3>
+
       <div className="my-4">
         <BlogPosts />
       </div>
