942da1bd391628608fec366b5612d98640af0cce
Jônatas Quirino <105068717+ojonatasquirino@users.noreply.github.com>
2025-03-27 13:01:51 +0000

    feat: update message of page not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,10 @@ export default function NotFound() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Página Não Encontrada
+        404 • Página Não Encontrada
       </h1>
       <p className="mb-4">
-        A página que você está acessando está em construção ou não existe :/
+        A página que você está acessando está em construção :)
       </p>
     </section>
   );
