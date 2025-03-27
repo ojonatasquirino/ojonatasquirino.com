72df4fbd664860afefc4bda860e2677dc32daaaa
Jônatas Quirino <105068717+ojonatasquirino@users.noreply.github.com>
2025-03-27 13:04:14 +0000

    feat: color correction

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Page() {
             className="border border-gray-300 dark:border-gray-400 rounded-xl p-4 shadow-sm hover:shadow-md transition bg-white dark:bg-black"
           >
             <div className="h-32 bg-white dark:bg-[#0a0a0a] rounded-md flex items-center justify-center">
-              <div className="h-32 bg-white dark:bg-[#1a1a1a] rounded-md flex items-center justify-center overflow-hidden">
+              <div className="h-32 bg-white dark:bg-[#0a0a0a] rounded-md flex items-center justify-center overflow-hidden">
                 <img
                   src="/brain.png"
                   alt="brain"
