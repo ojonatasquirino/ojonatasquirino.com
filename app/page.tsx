89fff383212f78bcf5614fd559d7b44131b61e1d
Jônatas Quirino <105068717+ojonatasquirino@users.noreply.github.com>
2025-03-31 23:39:59 +0000

    color correction

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
       <img
         src="/avatar.jpg"
         alt="Avatar de Jônatas Quirino"
-        className="w-16 h-16 mb-3 rounded-full border-2 border-white"
+        className="w-16 h-16 mb-3 rounded-full transition-transform duration-600 hover:rotate-6"
       />
 
       <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
@@ -34,7 +34,7 @@ export default function Page() {
         ].map((project, index) => (
           <div
             key={index}
-            className="border border-gray-300 dark:border-gray-400 rounded-xl p-4 shadow-sm hover:shadow-md transition bg-white dark:bg-black"
+            className="border border-gray-300 dark:border-black rounded-xl p-4 shadow-sm hover:shadow-md transition bg-white dark:bg-black"
           >
             <div className="h-32 bg-white dark:bg-[#0a0a0a] rounded-md flex items-center justify-center">
               <div className="h-32 bg-white dark:bg-[#0a0a0a] rounded-md flex items-center justify-center overflow-hidden">
@@ -47,14 +47,14 @@ export default function Page() {
             </div>
 
             <h3 className="text-lg font-semibold mt-4">{project.title}</h3>
-            <p className="text-sm text-gray-600 dark:text-gray-400">
+            <p className="text-sm text-gray-800 dark:text-gray-300">
               {project.description}
             </p>
             <div className="flex flex-wrap gap-2 mt-3">
               {project.tags.map((tag, i) => (
                 <span
                   key={i}
-                  className="px-2 py-1 text-xs font-medium bg-gray-200 dark:bg-gray-700 rounded-md"
+                  className="px-2 py-1 text-xs font-medium bg-gray-200 dark:bg-[#0a0a0a] rounded-md"
                 >
                   {tag}
                 </span>
