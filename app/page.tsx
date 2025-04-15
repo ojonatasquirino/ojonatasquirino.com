bb4fc83554c66374a398cf9335c4b349127ae93f
Jônatas Quirino <105068717+ojonatasquirino@users.noreply.github.com>
2025-04-15 03:42:47 +0000

    update bio on principal page

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,13 @@ export default function Page() {
         className="w-16 h-16 mb-3 rounded-full transition-transform duration-600 hover:rotate-6"
       />
 
-      <h1 className="mb-4 text-2xl font-semibold tracking-tighter">
+      <h1 className="mb-2 text-2xl font-semibold tracking-tighter">
         Jônatas Quirino
       </h1>
-      <p className="mb-3">{`Destilo meu cérebro construindo softwares úteis, design mínimo e percepções autênticas.
- `}</p>
+
+      <p className="mb-3">
+        {`Destilo meu cérebro construindo software, design mínimo e percepções autênticas.`}
+      </p>
       <div className="my-4">
         <BlogPosts limit={4} /> {/* Apenas os 4 artigos mais recentes */}
       </div>
