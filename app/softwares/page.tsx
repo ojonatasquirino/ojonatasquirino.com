f5b90ff4456917d98e5263b3b8372c797bb6c7b5
Jônatas Quirino <105068717+ojonatasquirino@users.noreply.github.com>
2025-04-19 21:27:08 +0000

    feat: add icon and link for deploy

--- app/softwares/page.tsx
+++ app/softwares/page.tsx
@@ -1,33 +1,39 @@
 "use client";
-import { Github } from "lucide-react";
+
+import { Github, ExternalLink } from "lucide-react";
+
 const projects = [
   {
     title: "ERP Mínimo",
     description:
-      "ERP mínimo que integra os setores de uma micro-empresa. Gestão e análise de faturamento, lucro e fluxo de caixa.",
+      "ERP que integra os setores fundamentais de uma micro-empresa: Análise de faturamento, custos e lucro.",
     tags: ["NextJS", "TypeScript", "TailwindCSS"],
     link: "https://github.com/ojonatasquirino/erp-minimo",
+    deploy: "https://erp-minimo.vercel.app",
   },
   {
     title: "Mercado",
     description:
-      "Listagem eficiente para compras fazer compras no mercado. Cria itens e itera unidades e preço.",
+      "Listagem eficiente para compras no mercado e shopping. Adiciona itens e suas respectivas quantidades e preços.",
     tags: ["NextJS", "TypeScript", "TailwindCSS"],
     link: "https://github.com/ojonatasquirino/mercado",
+    deploy: "https://mercado-lista.vercel.app",
   },
   {
     title: "Meta Financeira",
     description:
-      "Calcula quanto tempo o usuário a atingirá a meta estabelicida com a capacidade de economia mensal.",
+      "Calcula quanto tempo o usuário atingirá a meta estabelicida com a capacidade de economia mensal.",
     tags: ["NextJS", "TypeScript", "TailwindCSS"],
     link: "https://github.com/ojonatasquirino/meta-financeira",
+    deploy: "https://meta-financeira-blond.vercel.app",
   },
   {
     title: "Financeira Inteligente",
     description:
-      "Análise de reserva de emergência, meta de patrimônio milionário e investimento de renda fixa ao longo do tempo.",
+      "Análise de reserva de emergência, meta de patrimônio milionário e investimento de renda fixa.",
     tags: ["NextJS", "TypeScript", "TailwindCSS"],
     link: "https://github.com/ojonatasquirino/financeira-inteligente",
+    deploy: "#",
   },
 ];
 
@@ -41,9 +47,9 @@ export default function Page() {
         {projects.map((project, index) => (
           <div
             key={index}
-            className="border border-[#808080] dark:border-[#0a0a0a] rounded-xl p-4 shadow-sm hover:shadow-md transition bg-white dark:bg-black"
+            className="border border-gray-300/30 dark:border-white/10 rounded-xl p-4 shadow-sm hover:shadow-md transition bg-white dark:bg-black"
           >
-            <div className="h-32 bg-white dark:bg-[#0a0a0a] rounded-md flex items-center justify-center overflow-hidden">
+            <div className="border border-gray-300/30 dark:border-white/10 h-32 bg-white dark:bg-[#0a0a0a] rounded-md flex items-center justify-center overflow-hidden">
               <img
                 src="/brain.png"
                 alt="brain"
@@ -64,14 +70,24 @@ export default function Page() {
                 </span>
               ))}
             </div>
-            <a
-              href={project.link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center mt-4 text-sm font-medium text-gray-800 dark:text-gray-200 hover:underline"
-            >
-              <Github size={16} className="mr-2" /> GitHub
-            </a>
+            <div className="flex gap-4 mt-4 text-sm font-medium">
+              <a
+                href={project.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center text-gray-800 dark:text-gray-200 hover:underline"
+              >
+                <Github size={16} className="mr-2" /> GitHub
+              </a>
+              <a
+                href={project.deploy}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center text-gray-800 dark:text-gray-200 hover:underline"
+              >
+                <ExternalLink size={16} className="mr-2" /> Deploy
+              </a>
+            </div>
           </div>
         ))}
       </div>
